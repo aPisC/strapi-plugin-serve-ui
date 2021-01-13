6da6bf2cc489101c7605e36dc859ad61cad417a2
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-01-13 16:01:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Strapi plugin serve-ui
+
+This plugin will redirect fallback requests to a specified folder and index.html in it, or redirect requests to an another server.
+
+## Configuration
+- Enable `serve-ui` middleware and put it to the end of the loading order.
+- Customize settings in /config/serve-ui.js
+  - `path`: the path of the served folder if you want to use the file server mode.
+  - `proxy`: the host(string) or the port of the server if the content is served through a proxy.
---
+++ middlewares/serve-ui/index.js
@@ -0,0 +1,57 @@
+"use strict";
+
+/**
+ * Module dependencies
+ */
+
+// Node.js core.
+const fs = require("fs");
+const path = require("path");
+const koaStatic = require("koa-static");
+const convert = require("koa-connect");
+const proxy = require("http-proxy-middleware").createProxyMiddleware;
+
+/**
+ * Public assets hook
+ */
+
+module.exports = (strapi) => {
+  return {
+    async initialize() {
+      const config = strapi.config["serve-ui"];
+
+      if (config.proxy) {
+        const host =
+          typeof config.proxy === "number"
+            ? `localhost:${config.proxy}`
+            : config.proxy;
+
+        strapi.router.use(
+          convert(
+            proxy("/", {
+              target: `ws://${host}`,
+              ws: true,
+              changeOrigin: true,
+              secure: false,
+              logLevel: "error",
+            })
+          )
+        );
+      } else if (config.path) {
+        const { maxAge } = strapi.config.middleware.settings.public;
+        const buildDir = path.resolve(strapi.dir, config.path);
+
+        strapi.router.get(
+          "/(.*)",
+          koaStatic(buildDir, {
+            maxage: maxAge,
+          })
+        );
+        strapi.router.use((ctx) => {
+          ctx.type = "html";
+          ctx.body = fs.createReadStream(path.join(`${buildDir}/index.html`));
+        });
+      }
+    },
+  };
+};
